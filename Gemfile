75b3641b1dd9acaae0e35254e70752608d81d637
NoNameA 774 <nonamea774@gmail.com>
2014-02-12 02:32:39 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~>3.2.2"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+gem "middleman-minify-html"
+gem "middleman-blog", "~> 3.5.1"
+gem "builder"
+gem "middleman-deploy"
---
+++ config.rb
@@ -0,0 +1,110 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
+  activate :minify_html
+  activate :gzip
+  activate :asset_hash
+end
+
+
+activate :livereload
+activate :directory_indexes
+# page "/blog/*", :directory_index => false
+
+Time.zone = "Tokyo"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  blog.permalink = "{year}/{month}/{day}/{title}"
+  # Matcher for blog source files
+  blog.sources = "{year}-{month}-{day}-{title}"
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "article_layout"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+  
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "blog/page/{num}"
+end
+
+page "/blog/feed.xml", layout: false
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method       = :rsync
+  deploy.host         = 'pi'
+  deploy.path         = '/var/www'
+  deploy.port         = "10044"
+end
