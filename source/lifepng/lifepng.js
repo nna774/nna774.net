009a41bc6c49151cea376985ea107f2bae93609d
NoNameA 774 <nonamea774@gmail.com>
2014-04-25 01:42:35 +0900

    nna774/lifepng/lifepng.js の最新版をもってきた

--- source/lifepng/lifepng.js
+++ source/lifepng/lifepng.js
@@ -23,10 +23,10 @@ onload = function(){
         var Loader = function(expectedCnt, callback){
             var cnt = 0;
             return function(){
-                if(++cnt == expectedCnt){ callback(); }
-            }
+                if(++cnt === expectedCnt){ callback(); }
+            };
         };
-        var loader = Loader(5, function(){
+        var loader = new Loader(5, function(){
             document.getElementById("status").innerText = "準備完了";
             document.getElementById("status").style.display="none";
         });
@@ -40,15 +40,16 @@ onload = function(){
             var strs = document.getElementById("input").value.split("\n");
             var h = strs.length;
             var maxW = 0;
-            for(var i=0; i < strs.length; ++i){
+            var i = 0, j = 0;
+            for(i=0; i < strs.length; ++i){
                 maxW = Math.max(maxW, strs[i].length);
             }
             document.getElementById("board").width = maxW * width + (maxW-1) * padWidth;
             document.getElementById("board").height = h * width + (h-1) * padWidth;
 
-            for(var i=0; i < h; ++i){
-                for(var j=0; j < maxW; ++j){
-                    if(strs[i][j] == 1){
+            for(i=0; i < h; ++i){
+                for(j=0; j < maxW; ++j){
+                    if(strs[i][j] === "1"){
                         context.drawImage(alive, j * (width + padWidth), i * (width + padWidth));
                     }else{
                         context.drawImage(dead , j * (width + padWidth), i * (width + padWidth));
@@ -58,7 +59,7 @@ onload = function(){
                     }
                 }
                 if(i < h - 1){
-                    for(var j=0; j < maxW; ++j){
+                    for(j=0; j < maxW; ++j){
                         context.drawImage(horizon , j * (width + padWidth), (i + 1) * width + i * padWidth);
                         if(j < maxW - 1){
                             context.drawImage(padding, (j+1) * width + j * padWidth, (i + 1) * width + i * padWidth);
@@ -66,36 +67,21 @@ onload = function(){
                     }
                 }
             }
-        }
+        };
     }
 };
-
-// function toImage(){
-//     var canvas = document.getElementById('board');
-//     if (canvas.getContext('2d')) {
-//         document.getElementById("lifepngIMG").src = canvas.toDataURL();
-//         document.getElementById("lifepngIMG").alt = "";
-//     }
-// }
 
 lifepng.next = function (){
     function sumMoore(map, i, j){
         var sum = 0;
-        if(map[i-1] != null){
-            if(map[i-1][j-1] != null && map[i-1][j-1] == 1) ++sum;
-            if(map[i-1][ j ] != null && map[i-1][ j ] == 1) ++sum;
-            if(map[i-1][j+1] != null && map[i-1][j+1] == 1) ++sum;
+        for(var k=i-1; k <= i+1; ++k){
+            if(typeof map[k] !== "undefined"){
+                if(map[k][j-1] === "1") { ++sum; }
+                if(map[k][ j ] === "1" && k !== i) { ++sum; }
+                if(map[k][j+1] === "1") { ++sum; }
+            }
         }
-        if(map[i] != null){
-            if(map[i][j-1] != null && map[i][j-1] == 1) ++sum;
-            if(map[i][j+1] != null && map[i][j+1] == 1) ++sum;
-        }
-        if(map[i+1] != null){
-            if(map[i+1][j-1] != null && map[i+1][j-1] == 1) ++sum;
-            if(map[i+1][ j ] != null && map[i+1][ j ] == 1) ++sum;
-            if(map[i+1][j+1] != null && map[i+1][j+1] == 1) ++sum;
-        }
-            
+        
         return sum;
     }
     
@@ -103,38 +89,42 @@ lifepng.next = function (){
     var map = [];
     var h = strs.length;
     var maxW = 0;
-    for(var i=0; i < strs.length; ++i) maxW = Math.max(maxW, strs[i].length);
-    for(var i=0; i < h; ++i) map[i] = [];
+    var i = 0, j = 0;
+    for(i=0; i < strs.length; ++i) { maxW = Math.max(maxW, strs[i].length); }
+    for(i=0; i < h; ++i) { map[i] = []; }
 
-    for(var i=0; i < h; ++i){
-        for(var j=0; j < maxW; ++j){
+    for(i=0; i < h; ++i){
+        for(j=0; j < maxW; ++j){
             var sum = sumMoore(strs, i, j);
-            if(strs[i][j] == 1){
-                if(sum == 2 || sum == 3 ){ map[i][j] = '1'; }
+            if(strs[i][j] === "1"){
+                if(sum === 2 || sum === 3 ){ map[i][j] = '1'; }
                 else { map[i][j] = '0'; }
             }
             else{
-                if(sum == 3) { map[i][j] = '1'; }
+                if(sum === 3) { map[i][j] = '1'; }
                 else { map[i][j] = '0'; }
             }
         }
     }
-    for(var i=0; i < h; ++i) map[i] = map[i].join("");
+    for(i=0; i < h; ++i) { map[i] = map[i].join(""); }
     document.getElementById("input").value = map.join("\n");
     this.draw();
-}
+};
 
 lifepng.randomize = function(){
     var strs = document.getElementById("input").value.split("\n");
     var map = [];
     var h = strs.length;
     var maxW = 0;
-    for(var i=0; i < strs.length; ++i) maxW = Math.max(maxW, strs[i].length);
-    for(var i=0; i < h; ++i) map[i] = [];
-    for(var i=0; i < h; ++i)
-        for(var j=0; j < maxW; ++j)
-            map[i][j] = !(Math.random()+.5|0)+0;
-    for(var i=0; i < h; ++i) map[i] = map[i].join("");
+    var i = 0, j = 0;
+    for(i=0; i < strs.length; ++i) { maxW = Math.max(maxW, strs[i].length); }
+    for(i=0; i < h; ++i) { map[i] = []; }
+    for(i=0; i < h; ++i){
+        for(j=0; j < maxW; ++j) {
+            map[i][j] = !(Math.random()+0.5|0)+0;
+        }
+    }
+    for(i=0; i < h; ++i) { map[i] = map[i].join(""); }
     document.getElementById("input").value = map.join("\n");
     this.draw();
-}
+};
