af3e6129467dae18a142f1b5fcf0d5599f49b2c3
NoNameA 774 <nonamea.774@gmail.com>
2016-01-18 16:47:47 +0900

    add blog.sh

---
+++ blog.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+YEAR=`date +"%Y"`
+MONTH=`date +"%m"`
+DAY=`date +"%d"`
+TIME=`date +"%H:%M"`
+ZONE="JST"
+
+TITLE=$1
+if [ -z "${TITLE}" ]; then
+    echo need title
+    exit 255
+fi
+
+NAME="${DAY}-${TITLE}.md"
+DATE="${YEAR}-${MONTH}-${DAY} ${TIME} ${ZONE}"
+DIR="source/blog/${YEAR}/${MONTH}"
+
+mkdir -p "${DIR}"
+
+PATH="${DIR}/${NAME}"
+if [ -e "${PATH}" ]; then
+    echo already exist!
+    exit 255
+fi
+
+echo "---" > "${PATH}"
+echo "title: ${TITLE}" >> "${PATH}"
+echo "date: ${DATE}"  >> "${PATH}"
+echo "tags: "  >> "${PATH}"
+echo "---"  >> "${PATH}"
+
+echo "${PATH} created"
