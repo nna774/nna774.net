e3db3c392ab6234025d36419059369c9f75c51d1
Kugayama Nana <nonamea.774@gmail.com>
2021-09-04 05:33:11 +0900

    wip

--- components/head.tsx
+++ components/head.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import style from '../css/header.module.css'
 
 interface Props {
   title?: string;
@@ -7,14 +9,22 @@ interface Props {
 }
 
 export const MyHead: React.FC<Props> = ({ title, desc }) => {
+  const router = useRouter();
   return (
-    <Head>
-      <title>{title || 'いっと☆わーくす！'}</title>
-      <meta
-        name="description"
-        content={desc || "Kugayama Nana's web page. main contents is blog..."}
-      />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>{title || 'いっと☆わーくす！'}</title>
+        <meta
+          name="description"
+          content={desc || "Kugayama Nana's web page. main contents is blog..."}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <h1 className={style.h1}>
+          <a href={router.pathname}>{title || 'いっと☆わーくす！'}</a>
+        </h1>
+      </header>
+    </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,68 @@
-import type { NextPage } from 'next';
+import React from 'react';
+import type { NextPage, InferGetStaticPropsType } from 'next';
 import { MyHead } from '../components/head';
+import style from '../css/homesection.module.css';
 
-const Home: NextPage = () => {
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+type Section = {
+  name: string;
+  to: string;
+  desc: string;
+  faviconStyle?: string;
+};
+
+const sections: ReadonlyArray<Section> = [
+  {
+    name: 'rebuild-kitashirakawa',
+    to: 'https://scrapbox.io/rebuild-kitashirakawa/',
+    desc: 'こっちが日記となった。なんでもこちらに書いちゃうので、ブログが更新されない……。',
+  },
+  {
+    name: 'Blog',
+    to: '/blog/',
+    desc: '最近更新していない。',
+  },
+  {
+    name: 'Piet',
+    to: '/piet/',
+    desc: 'Pietを書いていた時期もあった。',
+    faviconStyle: style.pietFavicon,
+  },
+  {
+    name: 'Projects',
+    to: '/projects/',
+    desc: '最近更新していないもの(最近更新しているもののほうが少ない)',
+  }
+];
+function renderSection(section: Section): JSX.Element {
+  const fs = section.faviconStyle || style.defaultFavicon;
+  return (
+    <section key={section.name} className={style.section} >
+      <div className={fs}>
+        <a href={section.to}>{section.name}</a>
+      </div>
+      {section.desc}
+    </section>
+  );
+}
+
+const Home: NextPage<Props> = () => {
   return (
     <div>
       <MyHead />
       <main>
         <h2>index of nna774.net</h2>
-        <p></p>
+        <div className={style.flexContainer}>{sections.map(renderSection)}</div>
       </main>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
 };
 
 export default Home;
